34cddab507cf02a339268a2ebb1309705dc4db37
Karthick-LaptopWin <smekarthick2005@gmail.com>
2025-03-07 00:02:16 +0530

    added some stuff

--- src/main.py
+++ src/main.py
@@ -13,3 +13,11 @@ else:
 
 # to set output, print to shell in following syntax
 print(f"::set-output name=num_squared::{num ** 2}")
+print("**"*50)
+print("num", os.environ.get("INPUT_NUM"))
+print("to", os.environ.get("INPUT_TO_ID"))
+print("cc", os.environ.get("INPUT_CC_ID"))
+print("starttime", os.environ.get("INPUT_START_TIME"))
+print("endtime", os.environ.get("INPUT_END_TIME"))
+print("content", os.environ.get("INPUT_CONTENT"))
+print("failure", os.environ.get("INPUT_FAILURE"))
